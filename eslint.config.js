01a63765abda75a2adfa915fae8f9021a50e134a
CEhM <eduardo.hernandez@cuenti.com>
2026-01-04 01:05:40 -0500

    init and configure the project for start

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import eslintPluginAstro from 'eslint-plugin-astro'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig([
+  globalIgnores(['dist/', 'node_modules/', '.astro/']),
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ['**/*.astro'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      eslintPluginAstro.configs.recommended,
+      eslintPluginPrettier,
+      prettierConfig,
+    ],
+  },
+])
