1a12b7fbf12d18f8331809deec9f13052aa501da
CEhM <eduardo.hernandez@cuenti.com>
2026-01-08 23:48:59 -0500

    Remove unused import statement from Spanish translations file

--- src/lang/es.ts
+++ src/lang/es.ts
@@ -1,5 +1,3 @@
-import type { Translations } from '@/lang'
-
 export default {
   title: 'Codificador y Decodificador Base64',
   description: 'Una herramienta sencilla para codificar y decodificar cadenas en Base64 en línea.',
